cb6388e600178041f06a2c29e33b6485d27b893e
Ruslan Borovilov <194803905+pycz2@users.noreply.github.com>
2025-04-18 14:07:54 +0200

    Add some filtering and ordering

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
 
 from tasks.models import Task
@@ -7,3 +9,6 @@ from tasks.serializers import TaskSerializer
 class TasksViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["is_completed"]
+    ordering_fields = ["due_date", "priority"]
